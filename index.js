d9b7ff5d34acde584c05177265de5d7087da8bc9
Richard Spitkovsky <rjspitkovsky@gmail.com>
2017-11-15 17:54:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,20 @@ function theBeatlesPlay(musicians, instruments) {
   };
   return array
 }
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(facts) {
+  var punctuatedfacts = [] 
+  let i = 0 
+  while (i < facts.length) {
+    punctuatedfacts.push(facts[i] + "!!!")
+    i++
+  }
+  return punctuatedfacts
+}
